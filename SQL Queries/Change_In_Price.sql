31e5312075131e6650f0c721dba44bc2b6656911
HannahParker <76972202+HannahParker@users.noreply.github.com>
2021-06-29 15:10:19 -0600

    Delete SQL Queries directory

--- SQL Queries/Change_In_Price.sql
+++
@@ -1,7 +0,0 @@
-USE SENIOR_PROJECT;
-
-SELECT *
-FROM Inventory_Noble_Hierarch 
-WHERE Date >= (SELECT MAX(Date) FROM Inventory_Noble_Hierarch
-WHERE Date < (SELECT MAX(Date) FROM Inventory_Noble_Hierarch))
-
--- SQL Queries/Most_Recent_Data_Updated.sql
+++
@@ -1,6 +0,0 @@
-USE SENIOR_PROJECT;
--- server name LAPTOP-AC3LNAA4\SQLEXPRESS
-SELECT t1.* 
-FROM dbo.Inventory_Noble_Hierarch t1
-WHERE t1.[Date] = (SELECT MAX(t2.Date)
-                 FROM dbo.Inventory_Noble_Hierarch t2)
